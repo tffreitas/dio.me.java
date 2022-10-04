1c9286ef39a6de522a0184218b4994792c2d91fb
Thiago <thiagofreitas2882@gmail.com>
2022-10-04 18:05:32 -0300

    add Ex_2

---
+++ estrutura-de-repeticao/src/Exercicios/Ex2_Nota.java
@@ -0,0 +1,29 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+/*
+faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+ */
+
+ public class Ex2_Nota {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota > 10) {
+            System.out.println("Nota Inválida. Digite a Nota novamente: ");
+            nota = scan.nextInt();
+        }       
+    }
+
+
+ }
