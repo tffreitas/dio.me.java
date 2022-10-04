3f993b7812f62453358f763fa0f353c3c86b42eb
Thiago <thiagofreitas2882@gmail.com>
2022-10-04 17:37:20 -0300

    estruturaderepeticao

---
+++ estrutura-de-repeticao/src/Exercicios/Ex1_NomeEidade.java
@@ -0,0 +1,7 @@
+
+
+/*
+Faça um programa que leia conjuntos de dois valores, o primeiro representando 
+o nome do aluno e o segundo representando a idade.
+(Pare o programa inserindo o valor 0 no campo nome.)
+*/
