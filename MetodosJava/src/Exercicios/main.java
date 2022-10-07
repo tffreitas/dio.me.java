99399cbd1fc39353fbd4904bb5de316f0382880b
Thiago <thiagofreitas2882@gmail.com>
2022-10-07 13:30:20 -0300

    add

---
+++ MetodosJava/src/Exercicios/main.java
@@ -0,0 +1,30 @@
+package Exercicios;
+
+public class main {
+
+    private static final int _3 = 3;
+
+    public static void main(String[] args) {
+
+        //Calculadora
+        System.out.println("Exercicio Calculadora");
+        calculadora.soma(3, 6);
+        calculadora.subtracao(9, 1.8);
+        calculadora.multiplicacao(7, 8);
+        calculadora.divisao(5, 2.5);
+        /*
+        // Mensagem
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        // Emprestimo
+        System.out.println("Exercicio Empréstimo");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, parcelas: 5);
+        */
+    }
+    
+}
