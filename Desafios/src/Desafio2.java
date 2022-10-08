358008bbd755b0129d5e0dc311dde61dd024c1d1
Thiago <thiagofreitas2882@gmail.com>
2022-10-08 08:39:52 -0300

    desafio2

--- Desafios/src/Desafio2.java
+++ Desafios/src/Desafio2.java
@@ -4,14 +4,25 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class Desafio2 {
     public static void main(String[] args) {
         int A, N;
+        int multiplicador = 1;
+        int soma = 0;
 
         Scanner input = new Scanner(System.in);
+        //System.out.println("Digite o numero: ");
         A = input.nextInt();
+        //System.out.println("Digite o limitador");
         N = input.nextInt();
 
         //TODO:  Retorne o valor da soma de todos múltiplos de "A" até o seu limite "N".
 
-        
+        for(int count = A; count <= N; count = count + A) {            
+            //System.out.println(A*multiplicador);
+            soma = soma + A*multiplicador; 
+            multiplicador++;                     
+        }
+        System.out.println(soma);
+    }
+}
