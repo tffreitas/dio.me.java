aaf883ac9db5fb70e4df18aeadde698597cc880f
Thiago <thiagofreitas2882@gmail.com>
2022-10-10 18:15:50 -0300

    sistemademedida

---
+++ estrutura-de-repeticao/src/Exemplo/src/SistemaMedidaSwitcCase.java
@@ -0,0 +1,25 @@
+public class SistemaMedidaSwitcCase {
+
+    public static void main(String[] args) {
+        
+        String sigla = "P";
+
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M": {
+                System.out.println("MEDIO");
+                break;
+            }
+            case "G": {
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+        }
+    }
+    
+}
