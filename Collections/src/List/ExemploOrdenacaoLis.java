6cb87d81de0e24c9db90252ce9a448d7def1d783
Thiago <thiagofreitas2882@gmail.com>
2022-10-13 16:21:38 -0300

    add

--- Collections/src/List/ExemploOrdenacaoLis.java
+++ Collections/src/List/ExemploOrdenacaoLis.java
@@ -1,6 +1,9 @@
 package List;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /*
@@ -8,7 +11,7 @@ import java.util.List;
  * crie uma lista exibindo:
  * (nome - idade - cor);
  * 
- * Gato 1 = nome: Jon, idade: 18, cor: preta
+ * Gato 1 = nome: Jon, idade: 18, cor: preto
  * Gato 2 = nome: Simba, idade: 6, cor: tigrado
  * Gato 3 = nome: Jon, idade: 12, cor: amarelo
  */
@@ -22,22 +25,33 @@ public class ExemploOrdenacaoLis {
             add(new Gato("Jon", 12, "amarelo"));
             
         }};
-        
+
         System.out.println("--\tOrdem de Inserçao\t---");
+        System.out.println(meusGatos);       
 
         System.out.println("--\tOrdem aleatoria\t---");
+        Collections.shuffle(meusGatos);
+        System.out.println(meusGatos);
 
         System.out.println("--\tOrdem Natural (Nome)\t---");
+        Collections.sort(meusGatos);
+        System.out.println(meusGatos);
 
         System.out.println("--\tOrdem Idade\t---");
+        meusGatos.sort((Comparator<? super Gato>) new ComparatorIdade());
+        System.out.println(meusGatos);
 
         System.out.println("--\tOrdem cor\t---");
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
 
         System.out.println("--\tOrdem Nome/Cor/Idade\t---");
+        meusGatos.sort(new ComparatorNomeCorIdade());
+        System.out.println(meusGatos);
     }    
 }
 
-class Gato {
+class Gato implements Comparable<Gato> {
     private String nome;
     private Integer idade;
     private String cor;
@@ -61,11 +75,44 @@ class Gato {
     }
 
     public String toString() {
-        return "Gato{" +
+        return "{" +
         "nome= '" + nome + '\'' +
         ", idade= " + idade +
         ", cor= " + cor + '\'' +
         '}';        
+    }
+
+    @Override
+    public int compareTo(Gato gato) {
+        // TODO Auto-generated method stub
+        return this.getNome().compareToIgnoreCase(gato.getNome());
+    }
+}
+
+class ComparatorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return Integer.compare(g1.getIdade(), g2.getIdade());
+    }
+}
+
+class ComparatorCor implements Comparator<Gato> {
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+}
+
+class ComparatorNomeCorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        int nome = g1.getNome().compareToIgnoreCase(g2.getNome());
+        if (nome != 0) return nome;
+
+        int cor = g1.getCor().compareToIgnoreCase(g2.getCor());
+        if (cor != 0) return cor;
+        
+        return Integer.compare(g1.getIdade(), g2.getIdade());
     }
 }
 
